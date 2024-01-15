81e385ec95ada4acfe8ae4dd961cf2a8e02012c4
Kirttinath Ojha <kirttinathojha000@gmail.com>
2024-01-15 10:31:11 +0530

    api call and display

--- Dist/index.js
+++ Dist/index.js
@@ -1,2 +1,38 @@
 "use strict";
-console.log("Kirtti");
+const getUsername = document.querySelector("#user");
+const formSubmit = document.querySelector(".form");
+const main_container = document.querySelector(".main_container");
+//* Reusable function
+async function myCustomFetcher(url, options) {
+    const response = await fetch(url, options);
+    if (!response.ok) {
+        throw new Error(`Network response was not  ok - status: ${response.status}`);
+    }
+    const data = await response.json();
+    console.log(data);
+    return data;
+}
+//?lets display the card ui
+const showResultUI = (singleUser) => {
+    const { avatar_url, login, url } = singleUser;
+    main_container.insertAdjacentHTML("beforeend", `
+    <div class='card'>
+    <img src=${avatar_url} alt=${login}/>
+    <hr/>
+    <div class="card-footer">
+    <img src='${avatar_url}' alt="${login};
+    <a href="${url}"> Github </a>
+    
+    </div>
+    </div>
+    `);
+};
+function fetchUserData(url) {
+    myCustomFetcher(url, {}).then((userInfo) => {
+        for (const singleUser of userInfo) {
+            showResultUI(singleUser);
+        }
+    });
+}
+//* Default Function call
+fetchUserData("https://api.github.com/users");
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,60 @@
+const getUsername = document.querySelector("#user") as HTMLInputElement;
 
+const formSubmit = document.querySelector(".form") as HTMLFormElement;
+
+const main_container = document.querySelector(".main_container") as HTMLElement;
+
+//?Lets define contract of an object
+interface UserData {
+  id: number;
+  login: string;
+  avatar_url: string;
+  location: string;
+  url: string;
+}
+
+//* Reusable function
+async function myCustomFetcher<T>(
+  url: string,
+  options?: RequestInit
+): Promise<T> {
+  const response = await fetch(url, options);
+
+  if (!response.ok) {
+    throw new Error(
+      `Network response was not  ok - status: ${response.status}`
+    );
+  }
+  const data = await response.json();
+  console.log(data);
+
+  return data;
+}
+//?lets display the card ui
+const showResultUI = (singleUser: UserData) => {
+  const { avatar_url, login, url } = singleUser;
+  main_container.insertAdjacentHTML(
+    "beforeend",
+    `
+    <div class='card'>
+    <img src=${avatar_url} alt=${login}/>
+    <hr/>
+    <div class="card-footer">
+    <img src='${avatar_url}' alt="${login};
+    <a href="${url}"> Github </a>
+    
+    </div>
+    </div>
+    `
+  );
+};
+
+function fetchUserData(url: string) {
+  myCustomFetcher<UserData[]>(url, {}).then((userInfo) => {
+    for (const singleUser of userInfo) {
+      showResultUI(singleUser);
+    }
+  });
+}
+//* Default Function call
+fetchUserData("https://api.github.com/users");
